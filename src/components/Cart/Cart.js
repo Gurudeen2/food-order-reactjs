fb2ab8f9460d888a7602703ccc4a42eba156b2d9
ControlPlus <akeemtolanifatai@gmail.com>
2023-02-04 17:14:02 +0100

    update commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,13 +4,11 @@ import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      [
       {[{ id: "c1", name: "Sushi", price: 22.99, amount: 2 }].map(
         (cartItem) => {
           return <li key={cartItem.id}>{cartItem.name}</li>;
         }
       )}
-      ]
     </ul>
   );
   return (
@@ -20,10 +18,8 @@ const Cart = (props) => {
         <span>Total Amount </span>
         <span>16.99</span>
       </div>
-      <div className={classes.button}>
-        <button className={classes["button--alt"]} onClick={props.onCloseCart}>
-          Close
-        </button>
+      <div className={classes.actions}>
+        <button onClick={props.onCloseCart}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
