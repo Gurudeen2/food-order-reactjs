573599dd03d64acdb1e6e9fe253c4dfee734d825
ControlPlus <akeemtolanifatai@gmail.com>
2023-02-17 09:47:42 +0100

    new commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,9 +12,13 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const removeCartHandler = (id) => {};
+  const removeCartHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
-  const addCartHandler = (item) => {};
+  const addCartHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
