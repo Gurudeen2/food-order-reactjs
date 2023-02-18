34a229443e4440729783eb1d6e7fc92347b212b0
ControlPlus <akeemtolanifatai@gmail.com>
2023-02-18 20:51:03 +0100

    new commit

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../db/cart-context";
@@ -7,11 +7,26 @@ import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+
   const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
     return currentNum + item.amount;
   }, 0);
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+       setBtnHighlighted(true);
+    setTimeout(() => {
+      setBtnHighlighted(false)
+    }, 300);
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onShowCart}>
+    <button className={btnClasses} onClick={props.onShowCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
